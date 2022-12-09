017edc4deece5a9fc7f98e4b902f0decb8ef6e9b
Lê Khánh <lekhanh240895@gmail.com>
2022-12-09 19:48:18 +0700

    netflix first commit

---
+++ components/Header.tsx
@@ -0,0 +1,55 @@
+import Image from "next/image"
+import {
+    MagnifyingGlassIcon, BellIcon
+} from "@heroicons/react/24/solid"
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
+function Header() {
+    const [isScrolled, setIsScrolled] = useState(false)
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 0) {
+                setIsScrolled(true)
+            } else {
+                setIsScrolled(false)
+            }
+        }
+
+        window.addEventListener("scroll", handleScroll)
+        return () => {
+            window.removeEventListener("scroll", handleScroll)
+        }
+    }, [])
+
+    return (
+        <header className={`${isScrolled && "bg-[#141414]"}`}>
+            <div className="flex items-center space-x-2 md:space-x-10">
+                <Image src="https://images.ctfassets.net/4cd45et68cgf/7LrExJ6PAj6MSIPkDyCO86/542b1dfabbf3959908f69be546879952/Netflix-Brand-Logo.png?w=684&h=456" alt="" width={150} height={150}
+                    priority
+                    className="object-cover cursor-pointer"
+                />
+
+                <ul className="hidden md:flex space-x-4 items-center ">
+                    <li className="headerLink">Home</li>
+                    <li className="headerLink">TV Shows</li>
+                    <li className="headerLink">Movies</li>
+                    <li className="headerLink">New & Popular</li>
+                    <li className="headerLink">My List</li>
+                </ul>
+            </div>
+
+            <div className="flex items-center space-x-4 text-sm font-light">
+                <MagnifyingGlassIcon className="hidden h-6 w-6 text-white md:inline" />
+                <p className="hidden lg-inline">Kis</p>
+                <BellIcon className="h-6 w-6" />
+                <Link href="/account">
+                    <Image src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" alt="" className="w-6 h-6 cursor-pointer rounded" width={100} height={100} />
+                </Link>
+            </div>
+        </header>
+    )
+}
+
+export default Header
--- next.config.js
+++ next.config.js
@@ -2,6 +2,25 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.ctfassets.net",
+        pathname: "/4cd45et68cgf/**",
+      },
+      {
+        protocol: "https",
+        hostname: "upload.wikimedia.org",
+        pathname: "/wikipedia/**",
+      },
+      {
+        protocol: "https",
+        hostname: "image.tmdb.org",
+        pathname: "/t/p/**",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
