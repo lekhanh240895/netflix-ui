498c0c5e624445c5f53cb547ebd57b0799a53ff8
Lê Khánh <lekhanh240895@gmail.com>
2023-01-06 23:08:06 +0700

    ok

--- redux/selector.ts
+++ redux/selector.ts
@@ -1,5 +1,4 @@
 import type { RootState } from './store';
 
 export const appSelector = (state: RootState) => state.app;
-export const userSelector = (state: RootState) => state.user;
 export const authSelector = (state: RootState) => state.auth;
